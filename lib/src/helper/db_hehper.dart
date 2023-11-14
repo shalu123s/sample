e4887c6e3a8af8391b5c70ef2399daeaf7bc1f1d
shalu123s <ameenulrishal@gmail.com>
2023-11-14 12:22:28 +0530

    login using db

--- lib/src/helper/db_hehper.dart
+++ lib/src/helper/db_hehper.dart
@@ -1,13 +1,50 @@
+import 'package:pagination_app/model/user.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseHelper{
+
   final dbName ='localdb';
-  
+   static const table = 'notification_table';
+
+  static const columnId = 'id';
+  static const columnUser = 'user';
+  static const columnPass = 'password';
+
+    DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  factory DatabaseHelper() => instance;
+
+  DatabaseHelper.internal();
   Future<Database> intiDB()async{
    final dbpath =await getDatabasesPath();
    final path = join(dbpath,dbName);
-   return openDatabase(path,version: 1,onCreate: (db, version) { },);
+   return openDatabase(path,version: 1,onCreate: _onCreate,);
   }
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $columnUser TEXT NOT NULL,
+            $columnPass TEXT NOT NULL,
+          )
+          '''
+    );}
+
+    static Database? _database;
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+    _database = await intiDB();
+    return _database;
+  }
+
+Future<int?>insert(User user)async{
+Database? db =await instance.database;
+return await db?.insert(table, {
+columnUser:user.user,
+columnPass:user.password,
+});
+
+}
 
 }
--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pagination_app/model/user.dart';
 import 'package:pagination_app/src/screens/signup.dart';
 import '../helper/blockbutton.dart';
+import '../helper/db_hehper.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,6 +15,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController username = TextEditingController();
   final TextEditingController password = TextEditingController();
   final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+ final dbHelper = DatabaseHelper.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +83,7 @@ class TopContainer extends StatelessWidget {
   }
 }
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     super.key,
     required this.formkey,
@@ -93,31 +96,72 @@ class LoginForm extends StatelessWidget {
   final TextEditingController password;
 
   @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+late BuildContext _ctx;
+
+  bool _isLoading = false;
+
+  final formKey =  GlobalKey<FormState>();
+
+  final scaffoldKey =  GlobalKey<ScaffoldState>();
+
+   String? _username, _password;
+
+   LoginResponse? _response;
+
+   _LoginPageState() {
+    _response =  LoginResponse(this as LoginCallBack);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
+
+    void _showSnackBar(String text) {
+     ScaffoldMessenger.of(context). showSnackBar( SnackBar(
+      content:  Text(text),
+    ));
+  }
+  //   void _submit() {
+  //   final form = formKey.currentState;
+  //   if (form?.validate()) {
+  //     setState(() {
+  //       _isLoading = true;
+  //       form?.save();
+  //       _response.doLogin(_username, _password);
+  //     });
+  //   }else{
+  //     _showSnackBar('text');
+  //   }
+  // }
+  
+    return SizedBox(
       width: MediaQuery.of(context).size.width * 0.6,
       height: MediaQuery.of(context).size.height * 0.3,
       child: Form(
-        key: formkey,
+        key: widget.formkey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
-              controller: username,
+                onSaved: (val) => _username = val!,
+              controller: widget.username,
               keyboardType: TextInputType.emailAddress,
               validator: (input) => input?.contains('@') == false
                   ? "should be a valid email"
                   : null,
               decoration: InputDecoration(
                 labelText: 'email',
-                labelStyle: TextStyle(color: Theme.of(context).accentColor),
-                contentPadding: EdgeInsets.all(12),
+                labelStyle: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                contentPadding: const EdgeInsets.all(12),
                 hintText: 'johndoe@gmail.com',
                 hintStyle: TextStyle(
                     color: Theme.of(context).focusColor.withOpacity(0.7)),
                 prefixIcon: Icon(Icons.alternate_email,
-                    color: Theme.of(context).accentColor),
+                    color: Theme.of(context).colorScheme.secondary),
                 border: OutlineInputBorder(
                     borderSide: BorderSide(
                         color: Theme.of(context).focusColor.withOpacity(0.2))),
@@ -133,7 +177,8 @@ class LoginForm extends StatelessWidget {
               height: 30,
             ),
             TextFormField(
-              controller: password,
+                onSaved: (val) => _password = val!,
+              controller: widget.password,
               obscureText: false,
               validator: (input) => (input?.length ?? 0) < 6
                   ? 'should be more than 6 letters'
@@ -174,8 +219,14 @@ class LoginForm extends StatelessWidget {
               ),
               color: Colors.teal,
               onPressed: () {
-                if (formkey.currentState!.validate()) {
-                  // Handle login logic
+                if (widget.formkey.currentState!.validate()) {
+                   setState(() {
+              _isLoading = true;
+              _response?.doLogin(_username!, _password!);
+              });
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => const SigninScreen()),);
+                }else{
+                  _showSnackBar('text');
                 }
               },
             ),
@@ -184,5 +235,62 @@ class LoginForm extends StatelessWidget {
         ),
       ),
     );
+  }
+
+ Future<int?>onSave(User user)async{
+  DatabaseHelper db =DatabaseHelper();
+  var dbUser = await db.database;
+  int? res = await dbUser?.insert("User", user.toMap());
+    return  res;
+ }
+
+ Future<int?> deleteUser(User user) async {
+    DatabaseHelper db =DatabaseHelper();
+    var dbClient = await db.database;
+    int? res = await dbClient?.delete("User");
+    return res;
+  }
+
+   Future<User?> getLogin(String user, String password) async {
+        DatabaseHelper db =DatabaseHelper();
+    var dbClient = await db.database;
+    var res = await dbClient!.rawQuery("SELECT * FROM user WHERE username = '$user' and password = '$password'");
+    
+    if (res.isNotEmpty) {
+      return User.fromMap(res.first);
+    }
+    return null;
+  }
+
+   Future<List<User>?> getAllUser() async {
+            DatabaseHelper db =DatabaseHelper();
+
+    var dbClient = await db.database;
+    var res = await dbClient!.query("user");
+    
+    List<User>? list =
+        res.isNotEmpty ? res.map((c) => User.fromMap(c)).toList() : null;
+    return list;
+  }
+}
+abstract class LoginCallBack {
+  void onLoginSuccess(User user);
+  void onLoginError(String error);
+}
+class LoginResponse {
+  final LoginCallBack _callBack;
+  LoginRequest loginRequest =  LoginRequest();
+  LoginResponse(this._callBack);
+  doLogin(String username, String password) {
+    loginRequest
+        .getLogin(username, password)
+        .then((user) => _callBack.onLoginSuccess(user))
+        .catchError((onError) => _callBack.onLoginError(onError.toString()));
+  } 
+}
+class LoginRequest {
+ Future<User> getLogin(String username, String password) {
+    var result = getLogin(username,password);
+    return result;
   }
 }
